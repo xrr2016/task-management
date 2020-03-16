f9a832fe72b4c9a53cf6999b6d220681ca735d62
Cold Stone <xiaoranran1993@outlook.com>
2020-03-16 23:30:48 +0800

    feat: create tasks

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,28 @@
+import { Injectable }       from '@nestjs/common';
+import * as uuid            from 'uuid/v1';
+import { CreateTaskDto }    from "./dto/create.task.dto";
+import { Task, TaskStatus } from "./task.model";
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = []
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    }
+
+    this.tasks.push(task)
+    return task
+  }
+
+}
